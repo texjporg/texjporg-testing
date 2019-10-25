965541725b31eb25d95b668b0409bef16f1aa54d
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2019-10-26 06:45:52 +0900

    template

---
+++ build-config.lua
@@ -0,0 +1,18 @@
+-- Common settings for texjporg development repo, used by l3build script
+
+checkdeps   = checkdeps   or
+  {
+--    maindir .. "/some/subdirectory/of/dependent/package",
+  }
+
+checkengines    = checkengines
+  or {"pdftex", "xetex", "luatex", "ptex", "uptex"}
+checksuppfiles  = checksuppfiles  or
+  {
+    "regression-test.cfg",
+    "regression-test.tex"
+  }
+
+if checksearch == nil then
+  checksearch = false
+end
---
+++ build.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env texlua
+
+-- Build script for texjporg files
+
+-- No bundle or module here, but these have to be defined
+bundle  = "texjporg"
+module  = ""
+
+-- A couple of custom variables: the order here is set up for 'importance'
+bundles      = { }
+checkbundles =
+  {
+    "contrib"
+  }
+
+-- Location of main directory: use Unix-style path separators
+maindir = "."
+
+-- A custom main function
+function main(target)
+  local errorlevel
+  if target == "check" then
+    errorlevel = call(checkbundles, "check")
+  else
+    help ()
+  end
+  if errorlevel ~=0 then
+    os.exit(1)
+  end
+end
+
+-- Find and run the build system
+kpse.set_program_name("kpsewhich")
+if not release_date then
+  dofile(kpse.lookup("l3build.lua"))
+end
