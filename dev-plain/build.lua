4e804408b8737aebcc07f7efa00c4329550abf23
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2019-10-29 01:15:19 +0900

    add preliminary "printkanji.lvt"
      specialformats:
      - e[u]ptex: TeX Live binary
      - e[u]ptex-beta-empty: p3.8.90 with empty.tcx
      - e[u]ptex-beta-cp227: p3.8.90 with cp227.tcx

--- dev-plain/build.lua
+++ dev-plain/build.lua
@@ -1,5 +1,11 @@
 #!/usr/bin/env texlua
 
+-- for debugging
+--local orig_execute = os.execute
+--os.execute = function(...) 
+--  print("EXE", ...); return orig_execute(...)
+--end
+
 -- Build script for dev-plain packages
 
 -- Identify the bundle and module
@@ -17,9 +23,20 @@ checkdeps   =
   }
 checksearch = true
 
+specialformats = specialformats or {}
+specialformats.ep = specialformats.ep or
+{
+  ["eptex"] = {binary="eptex", format = "eptex", options="" },
+  ["euptex"] = {binary="euptex", format = "euptex", options="" },
+  ["eptex-beta-empty"] = {binary="eptex-beta", format = "eptex-beta", options="-translate-file=empty.tcx" },
+  ["eptex-beta-cp227"] = {binary="eptex-beta", format = "eptex-beta",  options="-translate-file=cp227.tcx" },
+  ["euptex-beta-empty"] = {binary="euptex-beta", format = "euptex-beta",  options="-translate-file=empty.tcx" },
+  ["euptex-beta-cp227"] = {binary="euptex-beta", format = "euptex-beta",  options="-translate-file=cp227.tcx" }
+}
+  
 checkengines    = checkengines
-  or {"eptex", "euptex"}
-checkformat  = "tex"
+  or {"eptex", "euptex", "eptex-beta-cp227", "euptex-beta-cp227","eptex-beta-empty", "euptex-beta-empty"}
+checkformat  = "ep"
 
 -- If we want to stop processing at the first error
 -- checkopts = "-interaction=batchmode -halt-on-error"
